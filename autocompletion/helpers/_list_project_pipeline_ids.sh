4c3ddd86e590ffd4fa34c49d1de835d0a89867a6
Alexis Lucattini <alexis.lucattini@umccr.org>
2023-05-30 14:59:26 +1000

    Added value hints to autocompletion
    Fixes #176

--- autocompletion/helpers/_list_project_pipeline_ids.sh
+++ autocompletion/helpers/_list_project_pipeline_ids.sh
@@ -3,11 +3,32 @@
 __config_setup.sh
 
 ## END CONFIG SETUP ##
-
-ATTRIBUTE_NAME="id"
-
-## INVOKE PROJECT PIPELINE ##
-
-__list_project_pipelines.sh
-
-## INVOKE PROJECT PIPELINE ##
+eval "$(\
+  curl   \
+    --fail   \
+    --silent   \
+    --location   \
+    --request 'GET'   \
+    --header 'Accept: application/vnd.illumina.v3+json'   \
+    --header "Authorization: Bearer ${ICAV2_ACCESS_TOKEN}"   \
+    --url "${ICAV2_BASE_URL-https://ica.illumina.com/ica/rest}/api/projects/${ICAV2_PROJECT_ID}/pipelines" | \
+  jq \
+    --raw-output \
+    --arg cols "$(tput cols)"\
+     '
+      .items |
+      (map(.pipeline.id | length) | max) as $max_pipeline_id |
+      (($cols | tonumber) - $max_pipeline_id - 4) as $max_description_length |
+      map(
+        [
+          "printf",
+          "%-*s -- %.*s\n",
+          $max_pipeline_id, .pipeline.id,
+          $max_description_length, "Code: \(.pipeline.code) Date: \(.pipeline.timeCreated) Description: \(.pipeline.description)"
+        ] |
+        @sh
+      )
+      |
+      .[]
+    ' \
+)"
